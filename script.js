56726e22fbde7e3697dd3bf0c7766cb766f4adbf
kerastgs <kerastugas@gmail.com>
2025-01-07 12:26:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 AOS.init();
 
+// Variabel untuk file audio
 var musik = "";
 
 var audio = document.querySelector(".audio");
 if (musik) {
-  audio.src = musik;
+    audio.src = musik;
 }
 
+// Fungsi untuk memulai tampilan
 function mulai() {
-  audio.play();
-  window.scrollTo(0, 0);
-  document.querySelector(".open").style = "opacity: 0;";
-  document.querySelector(".body").style = "overflow-y: scroll";
-  setTimeout(function() {
-    document.querySelector(".open").style.display = "none";
-  }, 1200);
+    audio.play();
+    window.scrollTo(0, 0); // Scroll ke atas
+    document.querySelector(".open").style.opacity = "0"; // Hilangkan layar pembuka
+    document.body.style.overflowY = "scroll"; // Aktifkan scroll
+    setTimeout(function () {
+        document.querySelector(".open").style.display = "none";
+    }, 1200); // Sembunyikan layar pembuka setelah 1,2 detik
 }
